0a0d7f3d01282dd2ac4f8c231db12d87cbfeb722
Razmanov666 <razmanov666@gmail.com>
2020-10-06 11:38:47 +0300

    refactor: game_functions.py

--- Python/Part2/alien_invasion/alien_invasion.py
+++ Python/Part2/alien_invasion/alien_invasion.py
@@ -14,6 +14,7 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
+        
     pygame.display.set_caption("Alien Invasion")
 
     # Создание объектов
