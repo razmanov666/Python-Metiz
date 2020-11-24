22cf99e5ab03b37df4cd858afab16ed137c7ef78
Alexey Razmanov <razmanov666@gmail.com>
2020-11-24 11:07:28 +0300

    feat: game statistic

--- Python/alien_invasion/classes/Settings.py
+++ Python/alien_invasion/classes/Settings.py
@@ -8,10 +8,11 @@ class Settings():
         """
         # Настройки корабля
         self.ship_speed_factor = 3
+        self.ship_limit = 3
 
         # Параметры пули
         self.bullet_speed = 7
-        self.bullet_width = 3000
+        self.bullet_width = 3
         self.bullet_height = 9
         self.bullet_color = (250 , 60, 0)
         self.bullets_allowed = 3
@@ -22,7 +23,7 @@ class Settings():
         self.bg_color = (0 , 60, 70)
 
         # Настройка пришельцев 
-        self.alien_speed_factor = 1
+        self.alien_speed_factor = 100
         self.fleet_drop_speed = 5
         self.speed_increment = 1.25
         # fleet_direction = 1 обозначает движение вправо, а -1 влево.
